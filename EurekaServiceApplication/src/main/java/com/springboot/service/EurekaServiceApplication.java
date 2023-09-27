23698c6eb996e252c820888a9bfd7917aa01a178
bhanumanoj <31289904+bhanumanoj@users.noreply.github.com>
2023-09-27 22:01:23 +0530

    Add files via upload

---
+++ EurekaServiceApplication/src/main/java/com/springboot/service/EurekaServiceApplication.java
@@ -0,0 +1,21 @@
+package com.springboot.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableEurekaServer
+// @EnableEurekaServer -> It will launch the Eureka Registration Service
+//		EnableEurekaServer -> It will launch the Eurkea Discovery Service
+//@EnableZuulProxy -> This will verify the URl based on ZUUL configuration.
+
+@SpringBootApplication
+public class EurekaServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServiceApplication.class, args);
+	}
+
+}
